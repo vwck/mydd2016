72683342f36c2fce0c202a672818fe9fbd5d19f3
Victor Wong <victorwong@outlook.my>
2016-09-23 00:12:31 +0800

    commit 2
    upload time at 12:12am

--- rawdata.php
+++ rawdata.php
@@ -4,16 +4,31 @@
 
 <body> 
 <?php
-$r; $r2;
+$r; $r2; $zero=0; $one=0; $two=0; $three=0; $four=0; $zone;
 echo "random for 10 iterations <br>";
 for ($i=1; $i <=10; $i++) {
 	           $r = rand(0,4);
+			       /* store zone values for analysis and next predictions */
+			          if ($r == 0) { $zero = $zero + 1; }
+			          if ($r == 1) { $one = $one + 1; }
+			          if ($r == 2) { $two = $two + 1; }
+			          if ($r == 3) { $three = $three + 1; }
+			          if ($r == 4) { $four = $four + 1; }
+
+
+					  
 			     if ($r >0) {
 					 for ($ii=1; $ii<$r; $ii++)
 					 { $r2 = rand(1,4); 
-						echo " ", $r2, " ";  }
+			          if ($r2 == 1) { $one = $one + 1; }
+			          if ($r2 == 2) { $two = $two + 1; }
+			          if ($r2 == 3) { $three = $three + 1; }
+			          if ($r2 == 4) { $four = $four + 1; }
+
+					      echo " ", $r2, " "; } /*end for loop*/
+					          
 					 
-				 }  /*endif*/
+				              }  /*endif*/
 
 
 			   
@@ -25,11 +40,16 @@ for ($i=1; $i <=10; $i++) {
 
 				echo $r, " <br> ";  
 				
-				}       	    
+				}  /*end for */     	    
              	  
+/* analysze data for next prediction - artifical intelligence */
 
-
-
+      echo "<br> zero = ", $zero, " one = ", $one, " two = ", $two, " three = ", $three, " four = ", $four;
+      echo "<br> The next prediction is in the highest priority to the lowest priority order <br>"; 
+      $zone = array ($zero, $one, $two, $three, $four );          
+      rsort($zone);
+        for ($y=0; $y<=4; $y++ ) 
+		{ echo " zone ", $zone[$y], "<br>";  }
 ?>
  
 </body>
